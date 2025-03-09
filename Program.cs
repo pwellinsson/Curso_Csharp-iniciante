e05e23331816c67749c6faacfb65d894b73e8f45
Pedro Wellinsson <pedrowellinsson368@gmail.com>
2025-03-09 10:26:00 -0300

    Insert common types of C#

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! \n \nHow are you?");
+Console.WriteLine("Hello, World!");
+
+/* Tipos mais comuns do C#
+bool - true ou false
+char - Um único caractere
+byte - Inteiro de 8 bits unsigned
+int - Inteiro de 32 bits
+long - Inteiro de 64 bits
+float - Número de ponto flutuante de 32 bits
+double - Número de ponto flutuante de 64 bits
+decimal - Número de ponto flutuante de 128 bits preciso (moeda)
+string - Sequência de caracteres
+
+- 8 bits unsigned (0 a 255) sem negativo
+- 32 bits (-2.147.483.648 a 2.147.483.647)
+- 64 bits (-9.223.372.036.854.775.808 a 9.223.372.036.854.775.807)
+- 128 bits (-79.228.162.514.264.337.593.543.950.335 a 7.922.8162.514.264.337.593.543.950.335)
+*/
+double b = 1.5;
+string a = "Hi Guys";
+Console.WriteLine(b);
+Console.WriteLine(a);
--- obj/Debug/net9.0/Curso_Csharp-iniciante.AssemblyInfo.cs
+++ obj/Debug/net9.0/Curso_Csharp-iniciante.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Curso_Csharp-iniciante")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb28ce8fb430623d097437ad1ebf99669e783c24")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9832fdb29512099a07a4a3122a047bc65ea2ae9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Curso_Csharp-iniciante")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Curso_Csharp-iniciante")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
